4591b9d31cc72860eb24fc697f83c90a0c1e0cc3
Mahkxai <shr.hardik@gmail.com>
2023-09-01 01:45:08 -0400

    Implemented basic lineaer operation functinalities and changed UI colors:

--- main.js
+++ main.js
@@ -1,61 +1,107 @@
+// TODO: DEL testcases
 
-// Prevent default keyboard action
-document.addEventListener("keydown", function(event) {
-    event.preventDefault(); 
-    console.log("Key pressed:", event.key);
-});
+// check if stack is empty
+function isEmpty(arr) {return (arr.length == 0);}
 
-// Get numpad elements on click
 const numpad = document.querySelectorAll(".numpad.num");
 const operator = document.querySelectorAll(".numpad.op");
 const lcd = document.getElementById("output");
 const prevResult = document.getElementById("prev-result");
 
+const num = ["", ""];
+let i, result, prevOp, on;
+
+function init() {
+    lcd.value = "";
+    prevResult.value = "";
+    num.fill("");
+    i = 0;
+    result = 0;
+    prevOp = "";
+    on = false;
+}
+
+init();
+
+// Prevent default keyboard action
+document.addEventListener("keydown", function(event) {
+    event.preventDefault(); 
+});
+
 // Parsing numbers
 numpad.forEach(button => {
   button.addEventListener('click', () => {
-    const num = button.textContent;
-    console.log(`Number clicked: ${num}`);
-    lcd.value += num;
+    const input = button.textContent;
+    num[i] += input;
+    lcd.value += input;
+    console.log(`Number clicked: ${lcd.value}`);
   });
 });
 
 // Parsing operators
+// process: empty -> val op val op -> result op val op -> result op val op -> result
 operator.forEach(button => {
-  button.addEventListener('click', () => {
-    const op = button.textContent;
-    console.log(`Operator clicked: ${op}`);
+    button.addEventListener('click', () => {
+        const op = button.textContent;
+        console.log(`Operator clicked: ${op}`);
+        
+        switch(op) {
+            case ".":
+                // temporary console logger
+                console.log(`Current Num: ${num[i]}\n`, 
+                    `Next Num: ${num[(i+1) % 2]}\n`,
+                    `On: ${on}\n`,
+                    `prevOp: ${prevOp}\n`
+                );
+                break;
+            case "AC":
+                init();
+                break;
+            case "backspace":
+                num[i] = num[i].slice(0,-1);
+                lcd.value = lcd.value.slice(0, -1);
+                break;
+            case "=":
+            default:
+                lcd.value += " " + op + " ";
+                let oldi = i;
+                i = (i+1) % 2;
+                
+                if (!on) {
+                    prevOp = op;
+                    on = true;
+                } else {
+                    switch(prevOp) {
+                        case "/":
+                            result = parseFloat(num[i]) / parseFloat(num[oldi]);
+                            break;
+                        case "*":
+                            result = parseFloat(num[oldi]) * parseFloat(num[i]);
+                            break;
+                        case "+":
+                            result = parseFloat(num[oldi]) + parseFloat(num[i]);
+                            break;
+                        case "-":
+                            result = parseFloat(num[i]) - parseFloat(num[oldi]);
+                            break;
+                    }
+                    prevOp = op;
+                    num[oldi] = result;
+                    num[i] = "";
 
-    switch(op) {
-        case "AC":
-            lcd.value = "";
-            break;
-
-        case "DEL":
-            // lcd.pop();
-            lcd.value = lcd.value.slice(0, -1);
-            break;
-
-        case "=":
-            if (lcd.value != "") {
-                prevResult.value = lcd.value;
-                lcd.value = "";
-            }
-            break;
-
-        case "/":
-        case "*":
-        case "+":
-        case "-":
-        case "%":
-        case "()":
-        case ".":
-            break;
-            
-        default:
-            lcd.value += " " + op + " ";
-    }
-  });
+                    if (op === "=") {
+                        prevResult.value = result;
+                        num[i] = result;
+                        num[oldi] = "";
+                        lcd.value = "";
+                    } else {
+                        lcd.value = Number(result.toFixed(4)) + " " + prevOp + " ";
+                    }
+                }  
+        }
+    
+    });
 });
+
 
 // add animation to shrink on click
